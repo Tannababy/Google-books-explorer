bea05a60c33723c871382199cd51c5d23b62789f
Lotanna <tannaloamobi@gmail.com>
2026-09-09 12:13:58 +0100

    Add: component structure and Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <h1>Google Books Explorer</h1>
+      <p>
+        Hi there, welcome to the Google books explorer. A search engine
+        specifically for finding your favourite books.
+      </p>
+    </>
+  );
+}
+
+export default Header;
