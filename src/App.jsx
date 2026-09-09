4dc642ead358bbe3f28809ca605e8c41b5057ed4
Lotanna <tannaloamobi@gmail.com>
2026-09-09 17:18:43 +0100

    Add: Card and CardList components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SearchForm from "./components/Form/Form";
+import CardList from "./components/CardList/CardList";
 
 function App() {
   return (
     <>
       <Header />
       <SearchForm />
+      <CardList />
     </>
   );
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+function Card(props) {
+  return (
+    <section>
+      {/* <image src={props.image} alt={props.title} />
+      <p>{props.title}</p>
+      <p>{props.author}</p>
+      <p>{props.description}</p> */}
+    </section>
+  );
+}
+export default Card;
